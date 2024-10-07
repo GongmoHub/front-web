9a69ddedc820625079fc730dea81cf77de34e11d
NanaManBo <kimbosung22@naver.com>
2024-10-07 22:02:00 +0900

    메인페이지 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
-export default function Home() {
+import './main.scss';
+
+const Mainpage: React.FC = () => {
   return (
     <div>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
-      <p>
-        hizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-      </p>
+      <div className="main_box_top">
+        <div className="information_box"></div>
+        <div className="project_box">
+          <input className="input_box"></input>
+          <div className="button_box">
+            <button className="button">피드백 요청</button>
+            <button className="button">공모전 검색</button>
+          </div>
+        </div>
+      </div>
+      <div className="main_box_middle"></div>
+      <div className="main_box_bottom"></div>
     </div>
   );
-}
+};
+
+export default Mainpage;

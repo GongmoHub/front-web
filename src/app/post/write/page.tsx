ebf43ccd50da6e9a517b7e5f267a3315db5a5655
SanghyunLee <lgw9736@naver.com>
2024-10-07 21:09:15 +0900

    ✅ post/write 뼈대

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -1,9 +1,56 @@
+'use client'
+
 import React from 'react'
-
-
+import './style.scss'
+import type { DatePickerProps } from 'antd';
+import { Input, DatePicker } from 'antd';
 
 export default function page() {
+  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
-    <div>page</div>
+    <div>
+      {/* 1 */}
+      <section>
+        <div className='section_title'>
+          <span className='order'>1</span>
+          <h2>참가할 공모전을 검색해주세요.</h2>
+        </div>
+        <div className='section_content'>
+          <Input placeholder="공모전을 검색해주세요" />
+        </div>
+      </section>
+      {/* 2 */}
+      <section>
+        <div className='section_title'>
+          <span className='order'>2</span>
+          <h2>프로젝트 기본 정보를 입력해주세요.</h2>
+        </div>
+        <div className='section_content'>
+          <ul className='list-line'>
+            <li className='list-item'>
+              <label htmlFor="">모집 인원</label>
+            </li>
+            <li className='list-item'>
+              <label htmlFor="">진행 방식</label>
+            </li>
+          </ul>
+          <ul className='list-line'>
+            <li></li>
+            {/* <DatePicker onChange={onChange} /> */}
+          </ul>
+        </div>
+
+      </section>
+      {/* 3 */}
+      <section >
+        <div className='section_title'>
+          <span className='order'>3</span>
+          <h2>프로젝트에 대해 소개해주세요.</h2>
+        </div>
+      </section>
+    </div>
   )
 }

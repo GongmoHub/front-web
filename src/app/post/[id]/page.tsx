1a2fb25650e39186bba7027001075b182ee58255
SanghyunLee <lgw9736@naver.com>
2024-10-08 16:11:39 +0900

    💄 post/[id] 중간 커밋

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import './style.scss'
 
 export default function page() {
+
+
+
   return (
     <div>
-      
+      <h1>post 제목</h1>
+
+      <div className='post-meta'>
+        <span>작성 날짜</span>
+      </div>
+
+      <div className='post-detail'>
+        <div>
+          <img src="https://image.fmkorea.com/files/attach/new4/20241008/7552974476_486616_00dd2ec0c93ca43e30ff71ec7c7d39cd.jpg" alt="공모전 포스터 이미지" />
+        </div>
+        <div className='detail-info'>
+          <span className='detail-info-title'>모집인원</span>
+        </div>
+      </div>
+
+      <div className='post-content'>
+        <p>글쓴 내용</p>
+      </div>
     </div>
   )
 }
